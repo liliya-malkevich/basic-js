14b89a25b308aa85fbe9b5a0eba55cfe7df84815
liliya-malkevich <l.malkevich12@gmail.com>
2021-02-07 16:29:25 +0300

    feat: added function carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,17 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
+module.exports = function dateSample(sampleActivity) {
 
   // remove line with error and write your code here
+ //console.log(typeof(sampleActivity));
+  if(arguments.length===0 || isNaN(Number(sampleActivity)) || typeof(sampleActivity) == "number" || Number(sampleActivity)===0 || typeof(sampleActivity)=== "object"){
+    return false;
+  }
+  sampleActivity = Number(sampleActivity);
+  let k = 0.693/HALF_LIFE_PERIOD;
+  let t = Math.log(MODERN_ACTIVITY/sampleActivity)/k;
+  return Math.floor(t);
 };
+//console.log(dateSample('1.1'));
+
