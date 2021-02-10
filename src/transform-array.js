624f8a3fcf8b18df170cf4d636226582a5cff8c3
liliya-malkevich <l.malkevich12@gmail.com>
2021-02-10 23:41:45 +0300

    feat: func

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
+module.exports = function transform(arr) {
   // remove line with error and write your code here
+  if(!Array.isArray(arr)){
+    throw new Error('THROW');
+  }
+  if(arr.length === 0) return [];
+  //if(typeof(arr[0]=== String)) delete arr[0];
+  //if(typeof(arr[arr.length - 1]=== String)) delete arr[arr.length-1];
+  return arr;
+  
 };
+//console.log(transform(5));
