4c5723755ab5ff9fe791b2fe0777ff3d36cbf14d
liliya-malkevich <l.malkevich12@gmail.com>
2021-02-16 23:53:28 +0300

    feat: added func, 1 wrong

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater( str, options ) {
+ // throw new CustomError('Not implemented');
+  // remove line with error and write your code he
+  str = String(str);
+  options.addition = String(options.addition);
+  if(options.repeatTimes === undefined) options.repeatTimes = 1;
+  if(options.additionRepeatTimes === undefined) options.additionRepeatTimes =0;
+  
+  if(options.separator === undefined) options.separator = '+';
+  if(options.addition == undefined){
+  options.addition = '';
+  options.additionRepeatTimes = 0;
+  }
+ 
+  //if(options.addition !== '' && options.additionRepeatTimes === 0) options.additionRepeatTimes = 1;
+  if(options.additionSeparator === undefined) options.additionSeparator = '|';
+  
+let  myStr = '';
+  for(let i = 0;i<options.repeatTimes;i++){
+    myStr += str;
+    for(let j = 0;j<options.additionRepeatTimes;j++){
+myStr +=options.addition;
+if(j !== options.additionRepeatTimes-1) myStr+= options.additionSeparator;
+    }
+    if(i !== options.repeatTimes-1)myStr+=options.separator;
+  }
+  
+return myStr;
 };
+//console.log(repeater('TESTstr', { repeatTimes: undefined, separator: 'ds', addition: 'ADD!', additionRepeatTimes: undefined, additionSeparator: ')))000' }))
   
