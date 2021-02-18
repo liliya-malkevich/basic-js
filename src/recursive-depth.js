b44569d6ea22770bc6d6ae0dbe787a23679eed6a
liliya-malkevich <l.malkevich12@gmail.com>
2021-02-18 22:05:03 +0300

    fix: fixed functions

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,14 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth( arr) {
+  calculateDepth( arr,d=0,temp = 0) {
     //throw new CustomError('Not implemented');
     // remove line with error and write your code here.
-   let d = 0;
+   
    if(arr.length == 0) return 1;
+
    for(let i of arr){
-     if(Array.isArray(i)) d += this.calculateDepth(i);
+     if(Array.isArray(i)) d = this.calculateDepth(i);
+     if(d>temp) temp = d;
    }
-   return d+1;
+   return temp+1;
   }
 };
--- src/what-season.js
+++ src/what-season.js
@@ -3,9 +3,12 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function getSeason(date ) {
   //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  
+  if(arguments.length === 0) return 'Unable to determine the time of year!';
+  if(typeof(date) != "object" || date.getDay.toString() != "function getDay() { [native code] }") throw new Error();
   if(date.getMonth()>=0 && date.getMonth()<=1 || date.getMonth() === 11) return 'winter';
   if(date.getMonth()>=2 && date.getMonth()<=4) return 'spring';
   if(date.getMonth()>=5 && date.getMonth()<=7) return 'summer';
   if(date.getMonth()>=8 && date.getMonth()<=10) return 'fall';
 };
-//console.log(getSeason((new Date(1602, 3, 19, 23, 25, 29, 673))));
+//console.log(getSeason(new Date(1456, 0, 2, 1, 50, 9, 238)));
