f05df566e013d3c1a9c4537a80e16aa41db384c6
liliya-malkevich <l.malkevich12@gmail.com>
2021-02-24 19:17:56 +0300

    feat: 4 wrong

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
+  flag = true;
+  constructor(flag = true){
+    this.flag = flag;
+  }
+  encrypt(message, key) {
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    if(arguments.length<2 || arguments[0]===undefined) throw new Error();
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
+  decrypt(message, key) {
+   // throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    if(arguments.length<2 || arguments[0]===undefined) throw new Error();
   }
 }
 
